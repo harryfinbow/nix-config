cce9028253434b684279fadecb131efc2041957b
Harry Finbow <64917871+harryfinbow@users.noreply.github.com>
2024-04-13 13:21:25 +0100

    home: rename from home-manager

--- home/nixos.nix
+++ home/nixos.nix
@@ -7,16 +7,8 @@
   pkgs,
   ...
 }: {
-  # You can import other home-manager modules here
   imports = [
-    # If you want to use home-manager modules from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModule
-
-    # You can also split up your configuration and import pieces of it here:
-    # ./nvim.nix
-
-    ./programs
-    ./programs/hyprland.nix
+    ./hyprland
   ];
 
   nixpkgs = {
