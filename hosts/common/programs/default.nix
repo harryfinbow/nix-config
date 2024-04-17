859772b3a712f6ce1579986708ba998dfe73fe2e
Harry Finbow <64917871+harryfinbow@users.noreply.github.com>
2024-04-17 22:02:41 +0100

    home: dump all installed packages

--- hosts/common/programs/default.nix
+++ hosts/common/programs/default.nix
@@ -1,1 +1,13 @@
-{ imports = [ ./fish.nix ./fonts.nix ./hyprland.nix ./nix.nix ]; }
+{ pkgs, config, ... }:
+
+{
+  imports = [ ./fish.nix ./fonts.nix ./hyprland.nix ./nix.nix ];
+
+  environment.etc."current-system-packages".text = let
+    packages = builtins.map (p: "${p.name}") config.environment.systemPackages;
+    sortedUnique =
+      builtins.sort builtins.lessThan (pkgs.lib.lists.unique packages);
+    formatted = builtins.concatStringsSep "\n" sortedUnique;
+  in formatted;
+
+}
