4b03a2b58fbde1177e5fd65ea762825b20c65e7b
Harry Finbow <harry@finbow.dev>
2024-05-25 16:40:42 +0100

    add neovim and fish abbrs

--- home/harryf/harryf-malt0.nix
+++ home/harryf/harryf-malt0.nix
@@ -1,36 +1,65 @@
 { pkgs, ... }:
 
 {
-  imports = [
-    ./default.nix
-  ];
+  imports = [ ./default.nix ];
+
+  home.sessionVariables.EDITOR = "nvim";
 
   home.packages = with pkgs; [
     awscli2
     tenv # tfenv for tofu + tfenv
     kubectl
+    kustomize
     docker
     colima
     poetry
+    eza
   ];
 
   programs.alacritty = {
     enable = true;
     settings = {
-      shell = {
-        program = "${pkgs.fish}/bin/fish";
-      };
+      shell = { program = "${pkgs.fish}/bin/fish"; };
       window.decorations = "none";
       font.size = 16;
     };
   };
 
-  programs.fish.enable = true;
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      vim = "nvim";
+      vi = "nvim";
+    };
+    shellAbbrs = {
+      ls = "eza";
+      ll = "eza -la";
+      cdr = "cd (git rev-parse --show-toplevel)";
+
+      k = "kubectl";
+      ka = "kubectl apply";
+      kd = "kubectl describe";
+      kg = "kubectl get";
+
+      gl = "git log --oneline -n 15";
+      gs = "git status";
+      gsw = "git switch";
+      gr = "git rebase";
+      gro = "git rebase origin/main";
+      gru = "git remote update";
+    };
+    plugins = [{
+      name = "pure";
+      src = pkgs.fishPlugins.pure.src;
+    }];
+  };
 
   programs.zoxide = {
     enable = true;
     enableFishIntegration = true;
   };
+
+  programs.neovim = { enable = true; };
 
   programs.ssh = {
     enable = true;
@@ -48,18 +77,13 @@
     userName = "Harry Finbow";
     userEmail = builtins.getEnv "GIT_EMAIL";
 
-    extraConfig.url."git@personal.github.com:harryfinbow".insteadOf = "git@github.com:harryfinbow";
+    extraConfig.url."git@personal.github.com:harryfinbow".insteadOf =
+      "git@github.com:harryfinbow";
 
-    includes = [
-      {
-        contents = {
-          user = {
-            email = "harry@finbow.dev";
-          };
-        };
+    includes = [{
+      contents = { user = { email = "harry@finbow.dev"; }; };
 
-        condition = "hasconfig:remote.*.url:git@github.com:harryfinbow/*";
-      }
-    ];
+      condition = "hasconfig:remote.*.url:git@github.com:harryfinbow/*";
+    }];
   };
 }
