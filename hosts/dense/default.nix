d670a3560a3bcbd62d7a9b60e1d440e8c18c17a7
Harry Finbow <harry@finbow.dev>
2024-05-28 10:55:34 +0100

    change default shell to fish (using chsh)

--- hosts/dense/default.nix
+++ hosts/dense/default.nix
@@ -1,40 +1,41 @@
 { pkgs, ... }:
 
 {
-    imports = [ ../common/programs/nix.nix ];
+  imports = [ ../common/programs/nix.nix ];
 
-    environment.systemPackages = with pkgs; [
-      home-manager
+  # https://github.com/LnL7/nix-darwin/issues/811
+  users.users.harry.shell = pkgs.fish;
 
-      # Move to home-manager once https://github.com/LnL7/nix-darwin/issues/139 is resolved
-      raycast
-      obsidian
-    ];
+  environment.systemPackages = with pkgs; [
+    home-manager
 
-    services.yabai = {
-      enable = true;
-      config = {
-        mouse_modifier = "cmd";
-        mouse_action1 = "move";
-        mouse_action2 = "resize";
+    # Move to home-manager once https://github.com/LnL7/nix-darwin/issues/139 is resolved
+    raycast
+    obsidian
+  ];
 
-        layout              = "bsp";
-        top_padding         = 10;
-        bottom_padding      = 10;
-        left_padding        = 10;
-        right_padding       = 10;
-        window_gap          = 10;
-      };
+  services.yabai = {
+    enable = true;
+    config = {
+      mouse_modifier = "cmd";
+      mouse_action1 = "move";
+      mouse_action2 = "resize";
+
+      layout = "bsp";
+      top_padding = 10;
+      bottom_padding = 10;
+      left_padding = 10;
+      right_padding = 10;
+      window_gap = 10;
     };
+  };
 
-    # Create /etc/zshrc that loads the nix-darwin environment.
-    programs.zsh.enable = true;
-    programs.fish.enable = true;
+  programs.fish.enable = true;
 
-    services.nix-daemon.enable = true;
+  services.nix-daemon.enable = true;
 
-    # The platform the configuration will be used on.
-    nixpkgs.hostPlatform = "aarch64-darwin";
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
 
-    system.stateVersion = 4;
+  system.stateVersion = 4;
 }
