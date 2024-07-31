f873ac275eaa4f0d056833efc363d8c2516ab175
Harry Finbow <harry@finbow.dev>
2024-07-31 23:34:40 +0100

    wayland: reduce night screen temp

--- home/common/wayland/default.nix
+++ home/common/wayland/default.nix
@@ -12,17 +12,9 @@
     enable = true;
     latitude = "51.51";
     longitude = "0.13";
-  };
-
-  # Gamescope Fix???
-  home.sessionVariables = {
-    GDK_BACKEND = "wayland,x11";
-    QT_QPA_PLATFORM = "wayland;xcb";
-    #SDL_VIDEODRIVER = "x11";
-    CLUTTER_BACKEND = "wayland";
-    XDG_CURRENT_DESKTOP = "Hyprland";
-    XDG_SESSION_TYPE = "wayland";
-    XDG_SESSION_DESKTOP = "Hyprland";
-    WLR_NO_HARDWARE_CURSORS = "1";
+    temperature = {
+      day = 6500;
+      night = 3000;
+    };
   };
 }
