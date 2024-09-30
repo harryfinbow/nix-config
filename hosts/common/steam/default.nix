52dcbd37f0aad6f6f8633aabcb9b181c90ce0ec4
Harry Finbow <harry@finbow.dev>
2024-09-30 21:41:33 +0100

    steam: add mangohud

--- hosts/common/steam/default.nix
+++ hosts/common/steam/default.nix
@@ -1,3 +1,18 @@
+{ pkgs, ... }:
+
 {
-  programs.steam.enable = true;
+  environment.sessionVariables = {
+    MANGOHUD = 1;
+  };
+
+  environment.systemPackages = with pkgs; [
+    mangohud
+  ];
+
+  programs.steam = {
+    enable = true;
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
+  };
 }
