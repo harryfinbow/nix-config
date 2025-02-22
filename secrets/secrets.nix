131d5a4cfb95aad982f2eb15473f23039a68565c
Harry Finbow <harry@finbow.dev>
2025-02-22 12:58:28 +0000

    secrets: rekey with new machines

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,10 @@
 let
-  hefty = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBhcj36L0yDUxWBWUo9MoxgrwnJGlm4VJGCsbBR8Owoc harry@hefty";
-  dense = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINPIM0ukzIdJpYpD6kNRCYkhh0G/UXVhSFKS3otW4VN+ harryf@dense";
+  alpha = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBhcj36L0yDUxWBWUo9MoxgrwnJGlm4VJGCsbBR8Owoc harry@alpha";
+  bravo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINPIM0ukzIdJpYpD6kNRCYkhh0G/UXVhSFKS3otW4VN+ harryf@bravo";
+  echo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILr+DZFexXxh3F4ioXrz/EX0hOqmH5t2etjldm8rEkfm harry@echo";
 in
 {
   # https://byamarmuric.gumroad.com/l/beautiful-topo-wallpaper-pack
-  "wallpaper.age".publicKeys = [ hefty ];
-  "work.git.age".publicKeys = [ dense ];
+  "wallpaper.age".publicKeys = [ alpha bravo echo ];
+  "work.git.age".publicKeys = [ alpha bravo echo ];
 }
