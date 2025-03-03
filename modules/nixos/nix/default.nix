6021d7a7078f9316abc140bf7a9b2f7fff2cfa42
Harry Finbow <harry@finbow.dev>
2025-03-03 18:55:38 +0000

    nix: enable automatic garbage collection

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -6,10 +6,18 @@
   };
 
   config = lib.mkIf config.modules.nix.enable {
-    nix.settings = {
-      experimental-features = "nix-command flakes";
-      auto-optimise-store = true;
-      use-xdg-base-directories = true;
+    nix = {
+      settings = {
+        experimental-features = "nix-command flakes";
+        auto-optimise-store = true;
+        use-xdg-base-directories = true;
+      };
+
+      gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 7d";
+      };
     };
 
     nixpkgs.config.allowUnfree = true;
