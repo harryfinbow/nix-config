b25a44bf08783fc64f0d44e1a9f4d958ad739ded
Harry Finbow <harry@finbow.dev>
2025-05-08 16:08:56 +0100

    secrets: add delta public key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,7 @@
 let
   alpha = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBhcj36L0yDUxWBWUo9MoxgrwnJGlm4VJGCsbBR8Owoc harry@alpha";
   bravo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINPIM0ukzIdJpYpD6kNRCYkhh0G/UXVhSFKS3otW4VN+ harryf@bravo";
+  delta = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDyVl7Sg9XYn6CCMdTOd+KJcuOLeW+vcU5Dpk5TbIuvF harry@delta";
   echo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILr+DZFexXxh3F4ioXrz/EX0hOqmH5t2etjldm8rEkfm harry@echo";
 in
 {
