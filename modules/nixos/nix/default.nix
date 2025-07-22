36632bd60fdb42f9fa51f4fddde850de60e7d7ae
Harry Finbow <harry@finbow.dev>
2025-07-22 15:26:27 +0100

    nix: add devenv cachix keys

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -18,6 +18,11 @@
         dates = "weekly";
         options = "--delete-older-than 7d";
       };
+
+      extraOptions = ''
+        extra-substituters = https://devenv.cachix.org
+        extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
+      '';
     };
 
     nixpkgs.config.allowUnfree = true;
