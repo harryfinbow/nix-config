72546876dc730882d10ac44de59ad201c4cca3b0
Harry Finbow <harry@finbow.dev>
2025-07-28 21:01:48 +0100

    users: add foxtrot to authorized ssh keys

--- modules/nixos/users/default.nix
+++ modules/nixos/users/default.nix
@@ -1,5 +1,4 @@
 {
-  lib,
   config,
   currentSystemUser,
   ...
@@ -20,6 +19,7 @@
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBhcj36L0yDUxWBWUo9MoxgrwnJGlm4VJGCsbBR8Owoc harry@alpha"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINPIM0ukzIdJpYpD6kNRCYkhh0G/UXVhSFKS3otW4VN+ harryf@bravo"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIITZ9CCMXyd2FZuAW6XgmRt6hQN7rLb8l1+0iexum/Rm harry@foxtrot"
       ];
     };
 
