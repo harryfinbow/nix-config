935ca81d083d0ddd5a696f8cba9886d35a9c0fcb
Harry Finbow <harry@finbow.dev>
2025-08-28 10:52:47 +0100

    darwin: add devenv cachix key

--- modules/darwin/nix/default.nix
+++ modules/darwin/nix/default.nix
@@ -12,6 +12,11 @@
         experimental-features = "nix-command flakes";
         use-xdg-base-directories = true;
       };
+
+      extraOptions = ''
+        extra-substituters = https://devenv.cachix.org
+        extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
+      '';
     };
 
     nixpkgs.config.allowUnfree = true;
