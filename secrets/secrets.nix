5d9ce9a516f64c8a7c3269b476499714ab54879e
Harry Finbow <harry@finbow.dev>
2025-08-29 14:32:19 +0100

    secrets: update echo ssh key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,7 +3,7 @@ let
   alpha = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBhcj36L0yDUxWBWUo9MoxgrwnJGlm4VJGCsbBR8Owoc harry@alpha";
   bravo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINPIM0ukzIdJpYpD6kNRCYkhh0G/UXVhSFKS3otW4VN+ harryf@bravo";
   delta = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDyVl7Sg9XYn6CCMdTOd+KJcuOLeW+vcU5Dpk5TbIuvF harry@delta";
-  echo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILr+DZFexXxh3F4ioXrz/EX0hOqmH5t2etjldm8rEkfm harry@echo";
+  echo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP2Ho76RvxTQoScXNWCj8xywOfzhcfnzlAcZj0Eis/3+ harry@echo";
   foxtrot = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIITZ9CCMXyd2FZuAW6XgmRt6hQN7rLb8l1+0iexum/Rm harry@foxtrot";
 
   # VMs
