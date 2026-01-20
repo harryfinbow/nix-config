74b87c695150a9820301e7fa91db666b77cf9aff
Harry Finbow <harry@finbow.dev>
2026-01-20 20:46:24 +0000

    waybar: add battery

--- modules/home/hyprland/waybar.nix
+++ modules/home/hyprland/waybar.nix
@@ -14,6 +14,7 @@
           modules-left = [ "hyprland/workspaces" ];
           modules-center = [ "hyprland/window" ];
           modules-right = [
+            "battery"
             "temperature"
             "network"
             "pulseaudio"
@@ -28,6 +29,22 @@
             };
           };
 
+          battery = {
+            states = {
+              warning = 30;
+              critical = 15;
+            };
+            format = "{capacity}% {icon}";
+            format-icons = [
+              " "
+              " "
+              " "
+              " "
+              " "
+            ];
+            max-length = 25;
+          };
+
           clock = {
             format = "{:%H:%M}";
             tooltip-format = "{:%Y-%m-%d}";
@@ -35,28 +52,28 @@
 
           network = {
             format = "{ifname}";
-            format-wifi = "  {signalStrength}%";
+            format-wifi = "{signalStrength}%  ";
             format-ethernet = " ";
             format-disconnected = "Not connected";
-            tooltip-format = "  {ifname} via {gwaddri}";
-            tooltip-format-wifi = "  {essid} ({signalStrength}%)";
-            tooltip-format-ethernet = "  {ifname} ({ipaddr}/{cidr})";
+            tooltip-format = "{ifname} via {gwaddri}  ";
+            tooltip-format-wifi = "{essid} ({signalStrength}%)  ";
+            tooltip-format-ethernet = "{ifname} ({ipaddr}/{cidr})  ";
             tooltip-format-disconnected = "Disconnected";
             interval = 15; # Update every 15 seconds
           };
 
           temperature = {
             hwmon-path = "/sys/class/hwmon/hwmon2/temp1_input"; # CPU Temperature
-            format = " {temperatureC}°C";
+            format = "{temperatureC}°C ";
           };
 
           pulseaudio = {
-            format = "{icon} {volume}%";
+            format = "{volume}% {icon}";
             format-muted = "";
             format-icons = {
               default = [
-                ""
-                ""
+                " "
+                " "
               ];
             };
             scroll-step = 1;
