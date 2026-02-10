2987c339db54e44ac673a790b85605504c4e342c
Harry Finbow <harry@finbow.dev>
2026-02-10 23:35:31 +0000

    foxtrot: reduce size of hyprland gaps

--- home/foxtrot/default.nix
+++ home/foxtrot/default.nix
@@ -4,5 +4,11 @@
     impermanence.enable = true;
   };
 
-  wayland.windowManager.hyprland.settings.monitor = "eDP-1, 1920x1080@60, 0x0, 1.25";
+  wayland.windowManager.hyprland.settings = {
+    monitor = "eDP-1, 1920x1080@60, 0x0, 1";
+    general = {
+      gaps_in = 5;
+      gaps_out = 10;
+    };
+  };
 }
