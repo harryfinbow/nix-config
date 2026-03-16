acaf3980cb88277deba072bcce309c19240e660d
Harry Finbow <harry@finbow.dev>
2026-03-16 22:32:02 +0000

    darwin: fix shell not being set

--- modules/users/harryf/default.nix
+++ modules/users/harryf/default.nix
@@ -3,10 +3,21 @@ let
   username = "harryf";
 in
 {
-  flake.modules.darwin.${username} = {
-    users.users.${username}.home = "/Users/${username}";
-    system.primaryUser = username;
-  };
+  flake.modules.darwin.${username} =
+    { config, ... }:
+    {
+      system.primaryUser = username;
+
+      users = {
+        users.${username} = {
+          home = "/Users/${username}";
+          uid = 503; # Should match `dscl . -read /Users/<username> UniqueID`
+        };
+
+        # https://github.com/nix-darwin/nix-darwin/issues/811#issuecomment-2227415650
+        knownUsers = [ username ];
+      };
+    };
 
   flake.modules.homeManager.${username} = {
     home = {
