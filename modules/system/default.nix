1ceac17a3e7bb649b103a2222fa371bcb611fd97
Harry Finbow <harry@finbow.dev>
2026-05-24 22:22:52 +0100

    system: increase sudo timeout to 60 minutes

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -2,5 +2,9 @@
   flake.modules.nixos.default = {
     time.timeZone = "Europe/London";
     i18n.defaultLocale = "en_GB.UTF-8";
+
+    security.sudo.extraConfig = ''
+      Defaults timestamp_timeout=60
+    '';
   };
 }
