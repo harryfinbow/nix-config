55cf632c14407b7651d0c6d5309905f3af029f4f
Harry Finbow <harry@finbow.dev>
2026-05-24 15:46:51 +0100

    users: add authorized keys to users

--- modules/users/harry/default.nix
+++ modules/users/harry/default.nix
@@ -16,6 +16,13 @@ in
           "networkmanager"
           "wheel"
         ];
+
+        # TODO: Work out a better way to do this
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBhcj36L0yDUxWBWUo9MoxgrwnJGlm4VJGCsbBR8Owoc harry@alpha"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINPIM0ukzIdJpYpD6kNRCYkhh0G/UXVhSFKS3otW4VN+ harryf@bravo"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGIbtwmjASEl5jw3btx0MVHf5MshDX9JT5EbwI9BXH3G harry@foxtrot"
+        ];
       };
     };
 
