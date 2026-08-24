5c8239367615c86e3343c5359fa149d37b362435
Harry Finbow <harry@finbow.dev>
2026-08-24 23:10:29 +0100

    vintagestory: temporarily disable

--- modules/hosts/node0/microvm.nix
+++ modules/hosts/node0/microvm.nix
@@ -5,48 +5,49 @@ let
 in
 {
   flake.modules.nixos.node0 = {
-    microvm.vms = {
-      vm0 = {
-        inherit pkgs;
-        config = topLevel.config.flake.modules.nixos.vm0;
-        extraModules = extraModules ++ [
-          {
-            # TODO: Sort this out (maybe make a root user)
-            users.users.root.openssh.authorizedKeys.keys = [
-              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBhcj36L0yDUxWBWUo9MoxgrwnJGlm4VJGCsbBR8Owoc harry@alpha"
-              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINPIM0ukzIdJpYpD6kNRCYkhh0G/UXVhSFKS3otW4VN+ harryf@bravo"
-              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGIbtwmjASEl5jw3btx0MVHf5MshDX9JT5EbwI9BXH3G harry@foxtrot"
-            ];
+    # TODO: Make this configurable
+    # microvm.vms = {
+    #   vm0 = {
+    #     inherit pkgs;
+    #     config = topLevel.config.flake.modules.nixos.vm0;
+    #     extraModules = extraModules ++ [
+    #       {
+    #         # TODO: Sort this out (maybe make a root user)
+    #         users.users.root.openssh.authorizedKeys.keys = [
+    #           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBhcj36L0yDUxWBWUo9MoxgrwnJGlm4VJGCsbBR8Owoc harry@alpha"
+    #           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINPIM0ukzIdJpYpD6kNRCYkhh0G/UXVhSFKS3otW4VN+ harryf@bravo"
+    #           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGIbtwmjASEl5jw3btx0MVHf5MshDX9JT5EbwI9BXH3G harry@foxtrot"
+    #         ];
 
-            microvm = {
-              vcpu = 4;
-              hotplugMem = 8192; # Maximum Memory (8 GiB)
-              hotpluggedMem = 4096; # Initial Memory (4 GiB)
+    #         microvm = {
+    #           vcpu = 4;
+    #           hotplugMem = 8192; # Maximum Memory (8 GiB)
+    #           hotpluggedMem = 4096; # Initial Memory (4 GiB)
 
-              shares = [
-                {
-                  source = "/persist/microvms/vintagestory";
-                  mountPoint = "/var/lib/vintagestory"; # TODO: Is there a way to access this from the service option?
-                  tag = "persist";
-                  proto = "virtiofs";
-                }
-              ];
+    #           shares = [
+    #             {
+    #               source = "/persist/microvms/vintagestory";
+    #               mountPoint = "/var/lib/vintagestory"; # TODO: Is there a way to access this from the service option?
+    #               tag = "persist";
+    #               proto = "virtiofs";
+    #             }
+    #           ];
 
-              interfaces = [
-                {
-                  id = "vm0";
-                  mac = "02:00:00:00:00:01";
-                  type = "macvtap";
-                  macvtap = {
-                    mode = "private";
-                    link = "enp1s0";
-                  };
-                }
-              ];
-            };
-          }
-        ];
-      };
-    };
+    #           interfaces = [
+    #             {
+    #               id = "vm0";
+    #               mac = "02:00:00:00:00:01";
+    #               type = "macvtap";
+    #               macvtap = {
+    #                 mode = "private";
+    #                 link = "enp1s0";
+    #               };
+    #             }
+    #           ];
+    #         };
+    #       }
+    #     ];
+    #   };
+    # };
   };
 }
